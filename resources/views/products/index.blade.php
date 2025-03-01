b1b05edcf528bbab3c942c9563ebbbd8ac73ec06
javniktool <nikitakremnev0@gmail.com>
2025-03-01 18:58:39 +0300

    add products and product category, tailwind and readme

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,29 @@
+@extends('welcome')
+
+@section('content')
+    <h1 class="text-3xl font-bold mb-4">Товары</h1>
+    <table class="table-auto w-full">
+        <thead>
+        <tr>
+            <th class="px-4 py-2">Название</th>
+            <th class="px-4 py-2">Описание</th>
+            <th class="px-4 py-2">Цена</th>
+            <th class="px-4 py-2">Действия</th>
+        </tr>
+        </thead>
+        <tbody>
+        <p>qweqwe</p>
+        @foreach ($products as $product)
+            <tr>
+                <td class="px-4 py-2">{{ $product->name }}</td>
+                <td class="px-4 py-2">{{ $product->description }}</td>
+                <td class="px-4 py-2">{{ $product->price }}</td>
+                <td class="px-4 py-2">
+                    <a href="#" class="text-gray-600 hover:text-gray-900 transition duration-300">Редактировать</a>
+                    <a href="#" class="text-gray-600 hover:text-gray-900 transition duration-300">Удалить</a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+@endsection
